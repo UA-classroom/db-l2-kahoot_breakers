e00f85dbbb7c52070a577258afcc1a562a319a86
Hassan Mehdi <hm85@protonmail.com>
2025-12-09 12:39:54 +0100

    Created SQL for all userinterface tables

--- db_setup.py
+++ db_setup.py
@@ -34,8 +34,54 @@ def create_tables():
     """
     A function to create the necessary tables for the project.
     """
+    subscriptions = """
+    CREATE TABLE IF NOT EXISTS subscriptions(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(20) NOT NULL
+    )
+    """
+
+    languages = """
+    CREATE TABLE IF NOT EXISTS languages(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(50) NOT NULL
+    )
+    """
+
+    customer_types = """
+        CREATE TABLE IF NOT EXISTS customer_types(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(30) NOT NULL
+    )
+    """
+
+    organisations = """
+        CREATE TABLE IF NOT EXISTS organisations(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(50) NOT NULL
+    )
+    """
+
+    users = """
+        CREATE TABLE IF NOT EXISTS users(
+        id SERIAL PRIMARY KEY,
+        username VARCHAR(50) NOT NULL UNIQUE,
+        email VARCHAR(255) NOT NULL UNIQUE,
+        password VARCHAR(255) NOT NULL,
+        birthdate DATE NOT NULL,
+        signup_date TIMESTAMP NOT NULL DEFAULT NOW(),
+        name VARCHAR(255),
+        subscriptions_id INT NOT NULL REFERENCES subscriptions(id) ON DELETE RESTRICT,
+        language_id INT NOT NULL REFERENCES languages(id) ON DELETE RESTRICT,
+        customer_type_id INT NOT NULL REFERENCES customer_types(id) ON DELETE RESTRICT,
+        organisation_id INT REFERENCES organisations(id) ON DELETE SET NULL
+    )
+    """
+
+
     connection = get_connection()
-    # Implement
+
+    
     pass
 
 
